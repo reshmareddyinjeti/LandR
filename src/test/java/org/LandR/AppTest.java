43ddac1283756cc00e2a6223397ad331e7e3471d
reshmareddyinjeti <47693116+reshmareddyinjeti@users.noreply.github.com>
2022-09-01 22:50:50 +0100

    Add files via upload

---
+++ src/test/java/org/LandR/AppTest.java
@@ -0,0 +1,21 @@
+package org.LandR;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("18 july");
+        System.out.println("1 st Augest");
+
+        assertTrue(true);
+    }
+}
